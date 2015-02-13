5de5198911ed6584d6672a89ff1eee43c58e819a
Unnar3 <unnar@unnar-Satellite-L650.(none)>
2015-02-13 11:35:46 +0100

    added configuration

--- include/exx_plane_extraction/plane_extraction_node.h
+++ include/exx_plane_extraction/plane_extraction_node.h
@@ -5,11 +5,8 @@
 
 namespace exx {
     namespace plane_extraction_node {
-        const std::string NODE_NAME =                 "plane_extraction";
-        const std::string TOPIC_PWM =                 "/arduino/pwm";
-        const std::string TOPIC_ENCODERS =            "/arduino/encoders";
-        const std::string TOPIC_TWIST =               "/s8/twist";
-        const std::string TOPIC_ACTUAL_TWIST =        "/s8/actual_twist";
+        const std::string NODE_NAME  = "plane_extraction";
+        const std::string CONFIG_DOC = "/home/unnar/catkin_ws/src/exx_plane_extraction/parameters/parameters.json";
     }
 }
 
